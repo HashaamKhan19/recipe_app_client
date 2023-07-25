f7c61a7b9c30abfda9425cb91fb936188ee9ad1e
hashaam <hashaamkhan4247@gmail.com>
2023-07-25 18:35:23 +0500

    save recipe implemented

--- src/components/Recipe/Card.jsx
+++ src/components/Recipe/Card.jsx
@@ -1,10 +1,34 @@
 /*eslint-disable*/
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { BallSpinner, ImpulseSpinner, StageSpinner } from "react-spinners-kit";
 
 const Card = ({ recipe, index }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSave = async (id) => {
+    const userID = localStorage.getItem("userID");
+
+    setLoading(true);
+    try {
+      const result = await axios.put("http://localhost:3000/recipes", {
+        id,
+        userID,
+      });
+      setLoading(false);
+      toast.success("Recipe saved successfully");
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error(error);
+    }
+  };
+
   return (
     <div
-      className="group bg-white rounded-lg shadow-sm border-[2px] p-4 max-w-[330px] cursor-pointer hover:shadow-md"
+      className="group bg-white rounded-lg shadow-sm border-[2px] p-4 max-w-[330px] hover:shadow-md"
       key={index}
     >
       <img
@@ -12,12 +36,31 @@ const Card = ({ recipe, index }) => {
         alt="Recipe"
         className="w-full h-48 object-cover rounded-md group-hover:scale-105 transition-all duration-300"
       />
-      <div className="py-4">
-        <h2 className="text-lg font-semibold">{recipe.name}</h2>
-        <p className="text-gray-600 mb-1">Created By: John Doe</p>
+      <div className="mt-1">
+        <div className="flex flex-row justify-between mt-2">
+          <h2 className="text-lg font-semibold">{recipe.name}</h2>
+          <button
+            className="bg-primary text-white rounded-sm px-2 mt-1"
+            onClick={() => handleSave(recipe._id)}
+          >
+            {loading ? <StageSpinner size={20} color={"#fff"} /> : "Save"}
+          </button>
+        </div>
         <p className="text-gray-600">
           Cooking Time: {recipe.cookingTime} minutes
         </p>
+        <div className="flex flex-row gap-2 items-center mt-2">
+          <p className="text-gray-600">Ingredients:</p>
+          <select className="border-2">
+            {recipe.ingredients.map((ingredient, idx) => {
+              return (
+                <option key={idx} value={ingredient}>
+                  {ingredient}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
     </div>
   );
